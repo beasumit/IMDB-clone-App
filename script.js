73b6c5a2923819cd2273d9695f66762ce8b6daca
Amit kumar <crazyone.amit>
2024-02-08 14:56:04 +0530

    try catch code done

--- script.js
+++ script.js
@@ -6,13 +6,15 @@ const key = "4f3b1290";
 
 // fetch url
 async function fetchMovieUrl(id) {
-//   const ids = "tt3896198";
-
-  const url = `http://www.omdbapi.com/?i=${id}&apikey=${key}`;
-  const res = await fetch(url);
-  const data = await res.json();
-  return data;
-
+  //   const ids = "tt3896198";
+  try {
+    const url = `http://www.omdbapi.com/?i=${id}&apikey=${key}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.error("error in fecthing", error);
+  }
   // Upon keypress - function findMovies is initiated
 }
 searchInput.addEventListener("input", findMovies);
@@ -20,12 +22,16 @@ searchInput.addEventListener("input", findMovies);
 // To display details of movies
 async function singleMovie() {
   // Finding ID of the movie from the URL
-  var urlQueryParams = new URLSearchParams(window.location.search);
-  var id = urlQueryParams.get("id");
-  const data = await fetchMovieUrl(id);
-  console.log(data);
+  try {
+    var urlQueryParams = new URLSearchParams(window.location.search);
+    var id = urlQueryParams.get("id");
+    const data = await fetchMovieUrl(id);
+    console.log(data);
+  } catch (error) {
+    console.error("Error is singleMovie", error);
+  }
 
-// result will display here
+  // result will display here
   var result = `
     <div class="movie-poster">
         <img src=${data.Poster} alt="Movie Poster">
@@ -68,38 +74,42 @@ async function addTofavorites(id) {
   console.log("fav-item", id);
 
   localStorage.setItem(Math.random().toString(36).slice(2, 7), id); // math.random for the unique key and value pair
-  alert("Movie Added to Watchlist!");
+  alert("Movie Added successfull to favorite");
 }
 
 //remove movies from website
 
 async function removeFromfavorites(id) {
   console.log(id);
+  try {
+    const keys = Object.keys(localStorage);
 
-  const keys = Object.keys(localStorage);
+    keys.forEach(function (key) {
+      //match the key with id and then remove it
+      if (localStorage[key] == id) {
+        localStorage.removeItem(key);
+      }
+    });
 
-  keys.forEach(function (key) {
-    //match the key with id and then remove it
-    if (localStorage[key] == id) {
-      localStorage.removeItem(key);
-    }
-  });
-
-  // Alert the user and refresh the page
-  alert("Movie Removed successfully");
-  window.location.replace("favorite.html");
+    // Alert the user and refresh the page
+    alert("Movie Removed successfully");
+    window.location.replace("favorite.html");
+  } catch (error) {
+    console.error("error in removeFromFavorite", error);
+  }
 }
 
 //Displaying the movie list on the search page according to the user list
 async function displayMovieList(movies) {
   // Map through the movies array and generate HTML for each movie
-  const result = movies
-    .map((movie) => {
+  try {
+    const result = movies
+      .map((movie) => {
         // poster
-      const img = movie.Poster !== "N/A" ? movie.Poster : "no movie";
-      const id = movie.imdbID;
+        const img = movie.Poster !== "N/A" ? movie.Poster : "no movie";
+        const id = movie.imdbID;
 
-      return `
+        return `
         <div class="fav-item">
           <div class="fav-poster">
             <a href="movie.html?id=${id}"><img src=${img} alt="Favourites Poster"></a>
@@ -116,45 +126,53 @@ async function displayMovieList(movies) {
             </div>
           </div>
         </div>`;
-    })
-    .join(""); 
+      })
+      .join("");
 
-  // connect to html page to display result
-  document.querySelector(".fav-container").innerHTML = result;
+    // connect to html page to display result
+    document.querySelector(".fav-container").innerHTML = result;
 
-  console.log("Here is the movie list:", movies);
+    console.log("Here is the movie list:", movies);
+  } catch (error) {
+    console.error("error is displayMovieList", error);
+  }
 }
 //list of movies display as per user search
 async function findMovies() {
-  const url = `https://www.omdbapi.com/?s=${searchInput.value.trim()}&page=1&apikey=${key}`;
-  const res = await fetch(`${url}`);
-  const data = await res.json();
+  try {
+    const url = `https://www.omdbapi.com/?s=${searchInput.value.trim()}&page=1&apikey=${key}`;
+    const res = await fetch(`${url}`);
+    const data = await res.json();
 
-  if (data.Search) {
-    // user search function is called.
-    displayMovieList(data.Search);
+    if (data.Search) {
+      // user search function is called.
+      displayMovieList(data.Search);
+    }
+  } catch (error) {
+    console.error("Error in findMovies", error);
   }
 }
 
 //Favorites movies is loaded here
 async function favoritesMovieLoader() {
-  var result = '';
-  for (i in localStorage) {
-    var id = await localStorage.getItem(i);
-    if (id != null) {
-      //Fetching the movie through id
-      const data = await fetchMovieUrl(id);
-      console.log(data);
+  try {
+    var result = "";
+    for (i in localStorage) {
+      var id = await localStorage.getItem(i);
+      if (id != null) {
+        //Fetching the movie through id
+        const data = await fetchMovieUrl(id);
+        console.log(data);
 
-      var img = "";
-      if (data.Poster) {
-        img = data.Poster;
-      } else {
-        img = data.Title;
-      }
-      var Id = data.imdbID;
-    
-      result += `
+        var img = "";
+        if (data.Poster) {
+          img = data.Poster;
+        } else {
+          img = data.Title;
+        }
+        var Id = data.imdbID;
+
+        result += `
 
         <div class="fav-item" aria-hidden="true">
             <div class="fav-poster">
@@ -175,8 +193,11 @@ async function favoritesMovieLoader() {
         </div>
 
        `;
+      }
     }
+    //connect with html page for favorites page
+    document.querySelector(".fav-container").innerHTML = result;
+  } catch (error) {
+    console.error("Error in favoritesMovieLoader", error);
   }
-  //connect with html page for favorites page
-  document.querySelector(".fav-container").innerHTML = result;
 }
